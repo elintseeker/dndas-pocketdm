ea7a2771170e8a1ab0e694cf5e37191278624f8b
kevinn <knavia@gmail.com>
2019-04-09 02:59:52 +0800

    add vue config for github and about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>Recently I've delved into tabletop games. At some point I have to get a dungeon crawl game to our table. The DnD Adventure System (DnD AS) is a good fit as it doesn't require a DM (Dungeon Master) and it plays fast (games lasts in an hour or so).
+    <p>This small vue.js app features some utilities for campaign play and/or randomization utilities. Plus additional house rules and stuff for any DnD AS game.</p>
+
+    <p>As usual, you can contribute and/or send feedback to the <a href="#">github repository</a>.</p>
   </div>
 </template>
