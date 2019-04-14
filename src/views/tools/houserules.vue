9d9f48590a04592c054e9a30eba09649508b6d0d
kevinn <knavia@gmail.com>
2019-04-14 11:22:10 +0800

    be consistent on elements

--- src/views/tools/houserules.vue
+++ src/views/tools/houserules.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="content">
     <div class="card">
-      <h2 class="subtitle is-5">Simple Initiative System I</h2>
+      <h2 class="xed-title is-serif">Simple Initiative System I</h2>
       <p><strong>Hero Phase:</strong> Moving to an unexplored edge triggers placement of a new tile. After drawing a new tile, place a monster on the new tile. Roll a d20 for your Hero and roll again for the Monster(s) side.</p>
       <ul>
         <li><strong>Active Hero rolled higher than the Monster(s)</strong> - Use the remaining movement points and/or attack actions, if applicable.</li>
         <li><strong>Monster(s) rolled higher than the Active Hero</strong> - Hero Phase ends and start the Villain Phase.</li>
       </ul>
 
-      <p>Notes: Active Hero can only explore one tile per turn.</p>
+      <p class="notes"><b>Notes:</b> Active Hero can only explore one tile per turn.</p>
     </div>
 
     <div class="card">
-      <h2 class="subtitle is-5">Simple Initiative System II</h2>
+      <h2 class="xed-title is-serif">Simple Initiative System II</h2>
       <p><strong>Hero Phase:</strong> Moving to an unexplored edge triggers placement of a new tile. After drawing a new tile, check the arrow marker of the new tile.</p>
       <ul>
         <li><strong>White arrowed tile</strong> - Active Hero has initiative. Use the remaining movement points and/or attack actions, if applicable.</li>
         <li><strong>Black arrowed tile</strong> - Monster(s) has initiative. Proceed sequence of play as normal.</li>
       </ul>
 
-      <p>Notes: Active Hero can only explore one tile per turn.</p>
+      <p class="notes"><b>Notes:</b> Active Hero can only explore one tile per turn.</p>
     </div>
 
     <div class="card">
-      <h2 class="subtitle is-5">Level Up/Progression System <small>(beyond Level 2)</small></h2>
-      <table class="table is-striped is-narrow is-bordered">
+      <h2 class="xed-title is-serif">Level Up/Progression System <small>(beyond Level 2)</small></h2>
+      <table class="table is-narrow is-bordered">
         <tr>
           <th>Level</th>
           <th>Cost</th>
@@ -54,8 +54,7 @@
         </tr> 
       </table>
 
-      <p>Notes: Take note of XP between adventures, as Monster Cards need to be shuffled to the Monster Deck when starting a new adventure.
-      You can only level up your Hero if all other heroes in the adventure are at equal levels. This is to avoid having a 5th level hero with 2nd level heroes in an adventure.</p>
+      <p class="notes"><b>Notes:</b> Take note and record of XP between adventures. You can only level up your Hero if all other heroes in the adventure are at equal levels. This is to avoid having a 5th level hero with 2nd level heroes in an adventure.</p>
     </div>
   </div>
 </template>
